4fdbc820dea0362c1a77504e29132c7084c6969a
theu <matheusvargas412@gmail.com>
2025-01-14 20:17:32 -0300

    add initial discriminator and bloomfilter classes

---
+++ src/BloomFilter.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+print("oi")
+
+
+class BloomFilter:
+    def __init__(self, size: int, num_hashes: int, dtype: type = bool, hash_fn=hash):
+        self.arr = np.zeros(size, dtype=dtype)
+        self.num_hashes = num_hashes
+        self.hash_fn = hash_fn
+
+    def add(self, item: str):
+        for i in range(self.num_hashes):
+            hash_val = self.hash_fn(item + str(i)) % len(self.arr)
+            self.arr[hash_val] += 1
+
+    def is_member(self, item: str) -> bool:
+        for i in range(self.num_hashes):
+            hash_val = self.hash_fn(item + str(i)) % len(self.arr)
+            if not self.arr[hash_val]:
+                return False
+        return True
---
+++ src/Discriminator.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+from BloomFilter import BloomFilter
+
+
+class Discriminator(ABC):
+    @abstractmethod
+    def get_response(self, input: str) -> int:
+        return 0
+
+    @abstractmethod
+    def train(self, input: str) -> None:
+        return None
+
+
+class DictDiscriminator(Discriminator):
+    def __init__(self):
+        self.dictionary = {}
+
+    def get_response(self, input: str) -> int:
+        return self.dictionary.get(input, 0)
+
+    def train(self, input: str):
+        if input in self.dictionary:
+            self.dictionary[input] += 1
+        else:
+            self.dictionary[input] = 1
+
+    def __str__(self):
+        return str(self.dictionary)
+
+
+class BloomDiscriminator(Discriminator):
+    def __init__(self, bloom_size: int, num_hashes: int):
+        self.bloom_filter = BloomFilter(bloom_size, num_hashes)
+
+    def get_response(self, input: str) -> int:
+        return self.bloom_filter.is_member(input)
+
+    def train(self, input: str):
+        self.bloom_filter.add(input)
+
+    def __str__(self):
+        return str(self.bloom_filter.arr)
