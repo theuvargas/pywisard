6e8ec67ebe384d23acb48385e1fc84a7d4266a64
theu <matheusvargas412@gmail.com>
2025-01-23 03:19:59 -0300

    add bloom wisard

--- src/Discriminator.py
+++ src/Discriminator.py
@@ -1,5 +1,5 @@
 import numpy as np
-from Node import DictNode
+from Node import BloomNode, DictNode
 
 
 class DictDiscriminator:
@@ -8,6 +8,31 @@ class DictDiscriminator:
         self.nodes = [DictNode() for _ in range(n_nodes)]
 
     def train(self, X_train: np.ndarray):
+        for row in X_train:
+            for i, node in enumerate(self.nodes):
+                tup = tuple(row[self.mapping[i]])
+                node.train(tup)
+
+    def get_response(self, row: np.ndarray, bleaching: int = 1) -> int:
+        indices = np.array([self.mapping[i] for i in range(len(self.nodes))])
+        tuples = [tuple(r) for r in row[indices]]
+        return sum(
+            node.get_response(tup, bleaching) for tup, node in zip(tuples, self.nodes)
+        )
+
+
+class BloomDiscriminator:
+    def __init__(
+        self,
+        n_nodes: int,
+        mapping: dict[int, np.ndarray],
+        bloom_size: int,
+        num_hashes: int,
+    ) -> None:
+        self.mapping = mapping
+        self.nodes = [BloomNode(bloom_size, num_hashes) for _ in range(n_nodes)]
+
+    def train(self, X_train: np.ndarray) -> None:
         for row in X_train:
             for i, node in enumerate(self.nodes):
                 tup = tuple(row[self.mapping[i]])
--- src/Wisard.py
+++ src/Wisard.py
@@ -1,5 +1,5 @@
 import numpy as np
-from Discriminator import DictDiscriminator
+from Discriminator import BloomDiscriminator, DictDiscriminator
 from utils import binarize_dataset
 
 
@@ -13,13 +13,79 @@ class DictWisard:
 
     def train(self, X_train: np.ndarray, y_train: np.ndarray):
         X_bin_train = binarize_dataset(X_train, self.bits_per_input)
-        print("finished binarizing")
         self._create_mapping(X_bin_train.shape[1])
 
         classes = np.unique(y_train)
         for c in classes:
             x = X_bin_train[y_train == c]
             discriminator = DictDiscriminator(self.n_nodes, self.mapping)
+            discriminator.train(x)
+            self.discriminators[c] = discriminator
+
+    def predict(self, X_test: np.ndarray) -> np.ndarray:
+        X_bin_test = binarize_dataset(X_test, self.bits_per_input)
+        y_pred = np.zeros(X_bin_test.shape[0])
+
+        for i, x in enumerate(X_bin_test):
+            scores = {}
+            bleaching = 1
+            while True:
+                for c, discriminator in self.discriminators.items():
+                    scores[c] = discriminator.get_response(x, bleaching)
+                max_val = max(scores.values())
+                if max_val == 0:  # prevents infinite loop
+                    bleaching -= 10
+                    for c, discriminator in self.discriminators.items():
+                        scores[c] = discriminator.get_response(x, bleaching)
+                    break
+                num_max = 0
+                for score in scores.values():
+                    if score == max_val:
+                        num_max += 1
+                        if num_max > 1:
+                            break
+                if num_max == 1:
+                    break
+                bleaching += 10
+
+            y_pred[i] = max(scores, key=scores.get)
+
+        return y_pred
+
+    def _create_mapping(self, input_size: int):
+        rng = np.random.default_rng()
+
+        for node in range(self.n_nodes):
+            self.mapping[node] = rng.integers(0, input_size, size=self.n_tuples)
+
+
+class BloomWisard:
+    def __init__(
+        self,
+        n_tuples: int,
+        n_nodes: int,
+        bits_per_input: int,
+        bloom_size: int,
+        num_hashes: int,
+    ):
+        self.n_tuples = n_tuples
+        self.n_nodes = n_nodes
+        self.bits_per_input = bits_per_input
+        self.bloom_size = bloom_size
+        self.num_hashes = num_hashes
+        self.discriminators = {}
+        self.mapping = {}
+
+    def train(self, X_train: np.ndarray, y_train: np.ndarray):
+        X_bin_train = binarize_dataset(X_train, self.bits_per_input)
+        self._create_mapping(X_bin_train.shape[1])
+
+        classes = np.unique(y_train)
+        for c in classes:
+            x = X_bin_train[y_train == c]
+            discriminator = BloomDiscriminator(
+                self.n_nodes, self.mapping, self.bloom_size, self.num_hashes
+            )
             discriminator.train(x)
             self.discriminators[c] = discriminator
 
